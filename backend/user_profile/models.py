4d93e7739d711758cebac2769e89a3f264bff192
kyli-Leo <li3758@purdue.edu>
2022-09-28 16:18:21 -0400

    Update models.py

--- backend/user_profile/models.py
+++ backend/user_profile/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+
+class User(AbstractUser):
+    isVerifiedUser = models.BooleanField
+    isResidenceManager = models.BooleanField
+    
