c420ce3311280d943e9f5b4d545c9404235fa300
kyli-Leo <li3758@purdue.edu>
2022-09-28 19:37:59 -0400

    Upate models

--- backend/user_profile/models.py
+++ backend/user_profile/models.py
@@ -7,3 +7,16 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     isVerifiedUser = models.BooleanField
     isResidenceManager = models.BooleanField
+
+
+class Residence(models.Model):
+    residenceName = models.CharField(max_length=100)
+    residenceManager = models.ForeignKey(User)
+
+class Review(models.Model):
+    reviewTitle = models.CharField(max_length=100)
+    reviewContent = models.CharField(max_length=10000)
+    reviewer = models.ForeignKey(User, on_delete=models.CASCADE)
+    reviewedResidence = models.ForeignKey(Residence, on_delete=models.CASCADE)
+
+
