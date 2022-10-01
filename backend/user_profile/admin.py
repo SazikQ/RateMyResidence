be49713ef86436277dca8733f07f1c466dde35a0
SazikQ <quan10@purdue.edu>
2022-10-01 19:30:59 -0400

    Fixed user model issues, added Custom User Admin form, custom creation form. registered new custom admin

--- backend/user_profile/admin.py
+++ backend/user_profile/admin.py
@@ -1,13 +1,20 @@
+from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
-
 # Register your models here.
-from.models import User
-from.models import Review
-from .models import Residence
-from .models import Location
+from .models import User, Review, Residence, Location
+from ..user_authentication.forms import CustomUserCreationForm, CustomUserChangeForm
 
 
-admin.site.register(User)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ("isVerifiedUser", "isResidenceManager")}),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Residence)
 admin.site.register(Review)
 admin.site.register(Location)
