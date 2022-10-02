6daccec9eef48281f06cfc7a11dd3e2385da46fb
YSW2000 <yang1949@purdue.edu>
2022-10-01 20:42:41 -0400

    changed template to main.html

--- rateMyResidence/urls.py
+++ rateMyResidence/urls.py
@@ -19,7 +19,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("", TemplateView.as_view(template_name="main.html"), name="home"),
     path("accounts/", include("backend.user_authentication.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
 
