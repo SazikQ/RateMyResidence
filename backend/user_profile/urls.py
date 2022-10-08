af45a2815dfdda362ec88120a8e1744a5388d899
YSW2000 <yang1949@purdue.edu>
2022-10-08 00:54:38 -0400

    added redirection to edit profile page

--- backend/user_profile/urls.py
+++ backend/user_profile/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from .views import EditProfileView
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', views.profile, name = "profile")
+    path('', views.profile, name = "profile"),
+    path('edit/', EditProfileView.as_view(), name = "editProfile")
 ]
--- backend/user_profile/views.py
+++ backend/user_profile/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from backend.user_authentication.forms import CustomUserChangeForm
+
 
 def profile(request):
     return render(request, 'account.html')
+
+
+class EditProfileView(CreateView):
+    form_class = CustomUserChangeForm
+    success_url = reverse_lazy('profile')
+    template_name = "editprofile.html"
