915d10c215a76ec61a99c8389997cab941b9d0ed
Alicesiwen <alice12192000@gmail.com>
2022-10-08 21:25:02 -0400

    comment

--- rateMyResidence/urls.py
+++ rateMyResidence/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-from backend.functions.views import AddResidenceView, SearchResultsView
+from backend.functions.views import AddResidenceView, SearchResultsView, add_residence
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
