3284675917344a35f525f05df97af02eaad16899
kyli-Leo <li3758@purdue.edu>
2022-10-13 17:19:58 -0400

    Update urls.py

--- backend/user_profile/urls.py
+++ backend/user_profile/urls.py
@@ -5,10 +5,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.profile, name = "profile"),
-<<<<<<< Updated upstream
-    path('edit/', views.edit_profile, name = "editProfile")
-=======
     path('edit/', views.edit_profile, name = "editProfile"),
     path('verify/', views.account_verify, name = "verifyUser")
->>>>>>> Stashed changes
 ]
