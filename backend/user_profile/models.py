e858ade0b08a1e8e867fa38752659201019227a5
jakobslason <jakobslason@gmail.com>
2022-10-23 02:19:12 -0400

    Filter by Rating

--- backend/user_profile/models.py
+++ backend/user_profile/models.py
@@ -1,6 +1,7 @@
 from email.policy import default
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models import Avg
 from taggit.managers import TaggableManager
 
 # Create your models here.
@@ -21,11 +22,19 @@ class Residence(models.Model):
     name = models.CharField(max_length=100)
     manager = models.ManyToManyField(User)
     location = models.OneToOneField(Location, on_delete=models.CASCADE)
+    rating_average = models.FloatField(default=0)
+    review_count = models.IntegerField(default=0)
     tags = TaggableManager()
 
     def __str__(self):
         return self.name
 
+    def update_review_fields(self):
+        reviews = self.comments.all()
+        #reviews = Review.objects.filter(id=self.id)
+        self.rating_average = reviews.aggregate(models.Avg('rating')).get('rating__avg')
+        self.review_count = reviews.count()
+        self.save(update_fields=['rating_average', 'review_count'])
 
 class Review(models.Model):
     publishTime = models.DateTimeField(auto_now_add=True)
@@ -42,4 +51,8 @@ class Review(models.Model):
     def __str__(self):
         # return super().__str__()
             return '%s - %s' % (self.belongedResidence.name, self.reviewer)
+    
+    def save(self, *args, **kwargs):
+        super(Review, self).save(*args, **kwargs)
+        self.belongedResidence.update_review_fields()
      
