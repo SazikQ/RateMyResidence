c86826839efaffc85adb64ddbd46678c7a2ad63e
kyli-Leo <li3758@purdue.edu>
2022-11-01 23:50:38 -0400

    activate email verification

--- rateMyResidence/settings.py
+++ rateMyResidence/settings.py
@@ -78,11 +78,11 @@ TEMPLATES = [
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.qq.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'joeldoe250@yahoo.com' #sender's email-id
-EMAIL_HOST_PASSWORD = 'zqxwcevrbt123@' #password associated with above email-id
+EMAIL_HOST_USER = 'ratemypurdueresidence@gmail.com' #sender's email-id
+EMAIL_HOST_PASSWORD = 'djlfrjcepcfoqiez' #password associated with above email-id
 
 WSGI_APPLICATION = 'rateMyResidence.wsgi.application'
 
